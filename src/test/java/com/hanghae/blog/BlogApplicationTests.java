87f4c582052e4e15f0da9598ab1b3f2df7996f2a
codenamehee <jhyfordev@gmail.com>
2021-09-29 23:55:29 +0900

    myhanghaeblog

---
+++ src/test/java/com/hanghae/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
